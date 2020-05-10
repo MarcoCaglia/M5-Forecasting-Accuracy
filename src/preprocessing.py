0b8ecb3d673e4617c3d7c5477add62e74e26af91
MarcoCaglia <cagliamarco@gmail.com>
2020-05-10 12:19:14 +0200

    feature: added logging
    s: changed chunksize

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,9 @@
 """Handles download and preprocessing of competition data."""
 
+import logging
 import sqlite3
 from zipfile import ZipFile
+from tqdm import tqdm
 
 import pandas as pd
 from kaggle import api
@@ -16,13 +18,23 @@ class Preprocessor:
         Arguments:
             data_path {str} -- Target path data storage.
         """
+        LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+        logging.basicConfig(
+            filename='preprocessing_log.log',
+            level=20,
+            format=LOG_FORMAT,
+            filemode='w'
+        )
+
+        logging.info('Initializing...')
+
         self.data_path = data_path
         self.zipfile = None
         self.file_generator = None
 
         self.connection = sqlite3.connect(data_path + '/m5.db')
 
-        self.api_download = api.download_cli
+        self.api_download = api.competition_download_cli
 
     def execute(self):
         """Execute Preprocessing pipeline."""
@@ -33,28 +45,33 @@ class Preprocessor:
         self.connection.close()
 
     def _download_zip(self):
+        logging.info('Starting download...')
         self.api_download(
             competition='m5-forecasting-accuracy', path=self.data_path
-            )
+        )
+        logging.info('Data obtained!')
 
     def _open_zip(self):
-        self.zipfile = ZipFile(self.data_path + 'm5-forecasting-accuracy.zip')
+        logging.info('Accessing...')
+        self.zipfile = ZipFile(self.data_path + '/m5-forecasting-accuracy.zip')
+        logging.info('Successfully accessed!')
 
     def _stream_to_db(self):
         filenames = (
             'calendar.csv',
             'sell_prices.csv',
             'sales_train_validation.csv'
-            )
+        )
 
         file_generator = (
-            (name, pd.read_csv(self.zipfile.open(name), chunksize=100))
+            (name, pd.read_csv(self.zipfile.open(name), chunksize=10000))
             for name in filenames
-            )
+        )
 
+        logging.info('Starting chunkwise upload...')
         upload = map(
-            lambda name, chunk_generator:
-            self._upload_file(name, chunk_generator), file_generator
+            lambda pair:
+            self._upload_file(pair[0], pair[1]), file_generator
         )
 
         _ = tuple(upload)
@@ -72,10 +89,11 @@ class Preprocessor:
             chunk.to_sql(name, **sql_config)
             if name != 'sales_train_validation.csv'
             else self._sales_to_long(chunk).to_sql(name, **sql_config),
-                chunk_generator
+                tqdm(chunk_generator)
         )
-
+        logging.info(f'Starting upload for {name}...')
         _ = tuple(uploader)
+        logging.info(f'Succesfully uploaded {name}!')
 
     @staticmethod
     def _sales_to_long(self, sub_table):
